0962dbfce24f150d0a3e0ba63e011e77e243ca26
Onur Aykac <onuraykac@gmail.com>
2016-12-14 22:31:59 +0200

    refactoring

--- app.py
+++ app.py
@@ -6,31 +6,35 @@ from bson import ObjectId
 
 app = Flask(__name__)
 
+
 @app.route("/api/add", methods=['POST'])
 @cross_origin()
 def add_phrases():
     phrases = get_phrases_coll()
-    requestjson = request.json["data"]
-    id = phrases.insert_one(requestjson).inserted_id
-    print("Object Id: "+str(id))
-    responsejson = {"en":requestjson["en"],"tr":requestjson["tr"],"id":str(id)}
-    return jsonify({"status":"ok","data":responsejson})
+    request_json = request.json["data"]
+    id = phrases.insert_one(request_json).inserted_id
+    print("Object Id: " + str(id))
+    response_json = {"en": request_json["en"], "tr": request_json["tr"], "id": str(id)}
+    return jsonify({"status": "ok", "data": response_json})
+
 
 @app.route("/api/list", methods=['GET'])
-@cross_origin
+@cross_origin()
 def get_all_phrases():
     phrases = get_phrases_coll()
     result = []
     data = phrases.find()
     for d in data:
-        result.append({"tr":d["tr"],"en":d["en"]})
-    return jsonify({"status":"ok", "data": result})
+        result.append({"tr": d["tr"], "en": d["en"]})
+    return jsonify({"status": "ok", "data": result})
+
 
 def get_phrases_coll():
     client = MongoClient("mongodb://dev:dev@ds119548.mlab.com:19548/nicespeaking")
     db = client.nicespeaking
     phrases = db['nice-phrases']
     return phrases
+
 
 if __name__ == "__main__":
     app.run()
